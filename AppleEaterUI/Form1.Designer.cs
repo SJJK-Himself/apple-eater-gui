69c5d8cfb98cde834145931d2c6f5c4eaa9daf9b
Sampo Kupiainen <sjjk@users.noreply.github.com>
2020-12-07 10:56:01 +0200

    Initial push

---
+++ AppleEaterUI/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AppleEaterUI
+{
+    partial class programWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.guideBox = new System.Windows.Forms.GroupBox();
+            this.guide1 = new System.Windows.Forms.Label();
+            this.feedButton = new System.Windows.Forms.Button();
+            this.guideBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 12);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(334, 90);
+            this.outputBox.TabIndex = 1;
+            this.outputBox.Text = "AI: Feed me an apple!";
+            this.outputBox.TextChanged += new System.EventHandler(this.OutputTextBox_handler);
+            // 
+            // guideBox
+            // 
+            this.guideBox.Controls.Add(this.guide1);
+            this.guideBox.Location = new System.Drawing.Point(12, 159);
+            this.guideBox.Name = "guideBox";
+            this.guideBox.Size = new System.Drawing.Size(334, 39);
+            this.guideBox.TabIndex = 4;
+            this.guideBox.TabStop = false;
+            this.guideBox.Text = "Guide";
+            this.guideBox.Enter += new System.EventHandler(this.GuideGroupBox_handler);
+            // 
+            // guide1
+            // 
+            this.guide1.AutoSize = true;
+            this.guide1.Location = new System.Drawing.Point(6, 16);
+            this.guide1.Name = "guide1";
+            this.guide1.Size = new System.Drawing.Size(223, 13);
+            this.guide1.TabIndex = 0;
+            this.guide1.Text = "Click the \'Feed\'-button to feed the AI an apple";
+            // 
+            // feedButton
+            // 
+            this.feedButton.Location = new System.Drawing.Point(139, 108);
+            this.feedButton.Name = "feedButton";
+            this.feedButton.Size = new System.Drawing.Size(75, 23);
+            this.feedButton.TabIndex = 5;
+            this.feedButton.Text = "Feed";
+            this.feedButton.UseVisualStyleBackColor = true;
+            this.feedButton.Click += new System.EventHandler(this.onFeedButtonClick);
+            // 
+            // programWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 210);
+            this.Controls.Add(this.feedButton);
+            this.Controls.Add(this.guideBox);
+            this.Controls.Add(this.outputBox);
+            this.Name = "programWindow";
+            this.Text = "Apple-eating AI";
+            this.guideBox.ResumeLayout(false);
+            this.guideBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.GroupBox guideBox;
+        private System.Windows.Forms.Label guide1;
+        private System.Windows.Forms.Button feedButton;
+    }
+}
+
---
+++ AppleEaterUI/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppleEaterUI
+{
+    public partial class programWindow : Form
+    {
+         int fed = 0;
+
+        public programWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputTextBox_handler(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GuideGroupBox_handler(object sender, EventArgs e)
+        {
+
+        }
+
+        private void onFeedButtonClick(object sender, EventArgs e)
+        {
+            if (fed < 100)
+                fed = fed + 1;
+            if (fed == 1)
+                outputBox.Text = "Thank you! You've fed me " + fed + " apple!";
+            if (fed > 1)
+                outputBox.Text = "Thank you! You've fed me " + fed + " apples!";
+            if (fed == 100)
+                outputBox.Text = "Thank you! You've fed me " + fed + " apples and I'm full now.";
+        }
+    }
+}
